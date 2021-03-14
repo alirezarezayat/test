e9263dbc62716378ee0f8cad8c7dc66a84dd0d44
alirezarezayat <alireza_rezayat1@yahoo.com>
2021-03-14 15:24:25 +0330

    commit

---
+++ test.js
@@ -0,0 +1,15 @@
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed another ${str}`;
+let y = -5;
+//alert(+y);
+//document.getElementById("id1").style.height="300px";
+//document.getElementsByClassName("id2").style.width="300px";
+let height = null;
+let width = null;
+let area = height ?? (100 * width) ?? 50;
+function salaam(){
+    document.getElementById("id1").style.width="300px";
+    document.getElementById("id1").style.height="300px";
+
+}
